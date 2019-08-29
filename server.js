470091dd76f54df8c2a828688e78d340bc9e4b40
micheal <micheal@almondcareers.com>
2019-08-30 00:48:31 +0100

    added some express middleware

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ mongoose.connect(config.url, {
 });
 
 // establish some middleware using express body parser(optionally i could just install body parser)
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 // set a port number
